30633cad36a1ddec350d5a936ac4e8466ff873c0
Ray Mulligan <rmullig2@optonline.net>
2023-12-04 15:18:07 -0500

    finished day3

--- day3/day3_part2.py
+++ day3/day3_part2.py
@@ -14,25 +14,35 @@ def make_border(input_list):
     formatted_input.append('.' * l)
     return formatted_input
 
-def check_top(row, col, numbers_indexes):
-    for i in range(col-1, col+2):
-        value = numbers_indexes.get((row-1,i))
+def check_numbers(row, col, numbers_indexes):
+    numbers_adjacent = []
+    value = numbers_indexes.get((row-1,col))
+    if value:
+        numbers_adjacent.append(value)
+    else:
+        value = numbers_indexes.get((row-1,col-1))
         if value:
-            return value
-    return None
-
-def check_bottom(row, col, numbers_indexes):
-    for i in range(col-1, col+2):
-        value = numbers_indexes.get((row+1,i))
+            numbers_adjacent.append(value)
+        value = numbers_indexes.get((row-1,col+1))
         if value:
-            return value
-    return None
-
-def check_left(row, col, numbers_indexes):
-    return numbers_indexes.get((row, col-1))
-
-def check_right(row, col, numbers_indexes):
-    return numbers_indexes.get((row, col+1))
+            numbers_adjacent.append(value)
+    value = numbers_indexes.get((row+1,col))
+    if value:
+        numbers_adjacent.append(value)
+    else:
+        value = numbers_indexes.get((row+1,col-1))
+        if value:
+            numbers_adjacent.append(value)
+        value = numbers_indexes.get((row+1,col+1))
+        if value:
+            numbers_adjacent.append(value)
+    value = numbers_indexes.get((row,col-1))
+    if value:
+        numbers_adjacent.append(value)
+    value = numbers_indexes.get((row,col+1))
+    if value:
+        numbers_adjacent.append(value)
+    return numbers_adjacent
 
 def add_to_indexes(row, col, num, numbers_indexes):
     start = col
@@ -60,15 +70,7 @@ def calculate_sum(formatted_input):
     for i in range(1, len(formatted_input)-1):
         for j in range(1, len(formatted_input[i])-1):
             if formatted_input[i][j] == "*":
-                numbers_adjacent = []
-                top_value = check_top(i, j, numbers_indexes)
-                bottom_value = check_bottom(i, j, numbers_indexes)
-                left_value = check_left(i, j, numbers_indexes)
-                right_value = check_right(i, j, numbers_indexes)
-                top_value and numbers_adjacent.append(top_value)
-                bottom_value and numbers_adjacent.append(bottom_value)
-                left_value and numbers_adjacent.append(left_value)
-                right_value and numbers_adjacent.append(right_value)
+                numbers_adjacent = check_numbers(i, j, numbers_indexes)
                 if len(numbers_adjacent) == 2:
                     sum_of_products += (numbers_adjacent[0] * numbers_adjacent[1])
     return sum_of_products
